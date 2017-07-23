1c16716fc3ac37ddad480d58e6587233a20ce5e1
sbal13 <sbal13@gmail.com>
2017-07-22 23:26:07 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,18 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def new
+  end
+
+  def create
+  	@student = Student.create(student_params)
+  	redirect_to @student
+  end
+
+  private
+
+  def student_params
+  	params.require(:student).permit(:first_name, :last_name)
+  end
+
 end
